fb315eb64b325e4b8f2f31e0293102103482fa4f
Clement Cattelle <gamisensei@gmail.com>
2014-11-03 19:17:50 +0100

    Re-ajout de la classe action , avec début de javadoc

---
+++ src/Action.java
@@ -0,0 +1,29 @@
+public abstract class Action {
+	private Coordonnees coord;
+	private Robot robot;
+	
+	/**
+     * renvoie le robot qui execute l'action
+     * @return Robot
+     */
+	public Robot getRobot(){
+		return this.robot;
+	}
+
+    /**
+     * renvoie la direction ou effectuer l'action (relatif au robot)
+     * @return Coordonnees
+     */
+	public Coordonnees getDirection(){
+		return this.coord;
+	}
+	
+	public Coordonnees getObjectif(){
+		
+	}
+	public Action(Robot robot,Coordonnees direction){
+		this.robot=robot;
+		this.coord=coord;
+	}
+	abstract void agit();
+}
