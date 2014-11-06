3ff0beb44ea9c1fe6c90d255b65c555d4e9f7f5d
groupe5-lille1 <groupe5-lille1@users.noreply.github.com>
2014-11-06 18:13:17 +0100

    Remplissage Constante.java

--- src/jeu/Constante.java
+++ src/jeu/Constante.java
@@ -1,4 +1,23 @@
 package jeu;
-public class Constante {
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jeu.plateau.Coordonnees;
+
+public class Constante {
+	
+	public static Coordonnees HAUT = new Coordonnees(0,-1);
+	public static Coordonnees BAS = new Coordonnees(0,1);
+	public static Coordonnees GAUCHE = new Coordonnees(-1, 0);
+	public static Coordonnees DROITE = new Coordonnees(1,0);
+	
+	public static List<Coordonnees> liste = new ArrayList<Coordonnees>();
+	static{
+		liste.add(BAS);
+		liste.add(HAUT);
+		liste.add(GAUCHE);
+		liste.add(DROITE);
+	}
 }
