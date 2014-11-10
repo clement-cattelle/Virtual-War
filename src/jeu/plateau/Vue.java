f35e3306628b8b749140fe8ce1443db81cd5fcc4
groupe5-lille1 <groupe5-lille1@users.noreply.github.com>
2014-11-10 16:20:11 +0100

    Enlevement doublons

--- src/jeu/plateau/Vue.java
+++ src/jeu/plateau/Vue.java
@@ -30,38 +30,3 @@ public class Vue {
 	}
 	
 }
-package jeu.plateau;
-public class Vue{
-	
-}package jeu.plateau;
-
-import jeu.entite.Robot;
-
-public class Vue {
-	
-	private int equipe;
-	private Plateau p;
-	
-	public Vue(int equipe,Plateau p){
-		this.equipe=equipe;
-		this.p=p;
-	}
-	
-	public void poserRobot(Robot r,Coordonnees c){
-		
-	}
-	
-	public boolean estOk(Coordonnees c){
-		if(!(c.getLargeur()>=0 || c.getLargeur()<p.getLargeur() || c.getHauteur()>=0 || c.getHauteur()<p.getHauteur())){
-			return false;
-		}
-		return true;
-	}
-	
-	public void poseMine(Robot r,Coordonnees c){
-		
-		
-		
-	}
-	
-}
