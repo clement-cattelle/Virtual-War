aa7f4c080912b9684a53b5a4ad7de1fb1796296d
groupe5-lille1 <groupe5-lille1@users.noreply.github.com>
2014-12-02 10:30:34 +0100

    Create Char.java

---
+++ src/jeu/entite/Char.java
@@ -0,0 +1,50 @@
+package jeu.entite;
+
+import java.util.List;
+
+import jeu.plateau.Coordonnees;
+import jeu.plateau.Vue;
+
+public class Char extends Robot{
+
+	public Char(Vue vue, int l, int h, int equipe) {
+		super(vue, l, h, equipe);
+		this.setEnergie(60);
+	}
+
+	
+	public boolean peutTirer() {
+		
+	}
+
+	
+	public int getCoutAction() {
+		return 1;
+	}
+
+	
+	public int getCoutDep() {
+		return 5;
+	}
+
+	
+	public int getDegatTir() {
+		return 6;
+	}
+
+	
+	public int getDegatMine() {
+		return 6;
+	}
+
+	
+	public String getType() {
+		return "Char";
+	}
+
+	
+	public List<Coordonnees> getDeplacements() {
+		return null;
+	}
+
+}
